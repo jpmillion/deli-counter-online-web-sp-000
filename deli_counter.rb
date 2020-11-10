ad6b6e713e8620ea9a60f56dacc1db6095a66bc5
John Million <jpmillion@comcast.net>
2020-11-10 03:15:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else  
+    current_place_in_line = array.map.with_index(1) {|name, index| "#{index}. #{name}"}
+    puts "The line is currently: " + current_place_in_line.join(" ")
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts array[-1] + array.index(name).to_s
+end
